a0f43d1a061c946d875487061d3b44f4586535ea
Esther de Freitas <estherdefreitas@gmail.com>
2025-11-07 16:11:21 -0300

    chore: initial project setup
    - add README.md
    - add project directories
    - add build.gradle
    - add .gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "br.edu.ifal"
+version = "0.0.1-SNAPSHOT"
+description = "StudyTrack é uma aplicação Web que auxilia estudantes no planejamento e acompanhamento de estudos. Ela permite cadastrar disciplinas, criar planos de estudo e registrar tarefas, monitorando o progresso de cada curso."
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/br/edu/ifal/devops/StudytrackApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudytrackApplication.class, args);
+	}
+
+}
